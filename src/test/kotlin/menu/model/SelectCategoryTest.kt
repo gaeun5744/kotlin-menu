2dbb8d85490fafa646b58a461dfd73b715ee02c1
gaeun <geun5744@gmail.com>
2023-12-12 15:54:41 +0900

    [TEST(SelectCategoryTest)] 한 주의 추천 카테고리 목록 반환 테스트

---
+++ src/test/kotlin/menu/model/SelectCategoryTest.kt
@@ -0,0 +1,32 @@
+package menu.model
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import menu.util.Category
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SelectCategoryTest {
+
+    @Test
+    fun `주간 카테고리 추천 반환 테스트`() {
+
+        val selectCategory = SelectCategory()
+
+        assertRandomNumberInRangeTest(
+            {
+                assertEquals(
+                    selectCategory.getWeekCategories(),
+                    listOf(
+                        Category.CHINESE,
+                        Category.KOREA,
+                        Category.CHINESE,
+                        Category.WESTERN,
+                        Category.WESTERN,
+                    )
+                )
+            },
+            3, 2, 3, 3, 5, 5, 5, 3,
+        )
+    }
+
+}
