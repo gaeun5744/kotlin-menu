8698c6ad7e34df9ee50431f6a5a8fea69a76cf71
gaeun <geun5744@gmail.com>
2023-12-12 17:34:13 +0900

    [FEAT(Validation)] 먹지 못하는 음식 입력 개수 체크 메서드 구현

--- src/main/kotlin/menu/util/Validation.kt
+++ src/main/kotlin/menu/util/Validation.kt
@@ -1,12 +1,11 @@
 package menu.util
 
-import java.lang.IllegalArgumentException
-
 object Validation {
 
     private const val COACH_COUNT_ERROR_MESSAGE = "[ERROR] 코치는 쉼표로 구분하여 최소 2명 이상 입력해야 합니다."
     private const val COACH_NAME_ERROR_MESSAGE = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자까지 가능합니다."
     private const val INVALID_MENU_ERROR_MESSAGE = "[ERROR] 유효하지 않은 메뉴 입력 입니다."
+    private const val MENU_COUNT_ERROR_MESSAGE = "[ERROR] 먹지 못하는 메뉴는 2개까지 가능합니다."
     private const val NAME_SEPARATOR = ","
 
     fun checkCoachesName(input: String) {
@@ -48,5 +47,12 @@ object Validation {
         }
 
         throw IllegalArgumentException(INVALID_MENU_ERROR_MESSAGE)
+    }
+
+    private fun checkMenuCount(input: String) {
+        val menus = input.split(NAME_SEPARATOR)
+        require(menus.size <= 2) {
+            MENU_COUNT_ERROR_MESSAGE
+        }
     }
 }
