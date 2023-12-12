63e86815b1195655f09e27cc4d4faaa086b6be53
gaeun <geun5744@gmail.com>
2023-12-12 17:51:28 +0900

    [TEST(ValidationTest)] 먹지 못하는 메뉴 입력 유효성 검증 테스트

--- src/test/kotlin/menu/util/ValidationTest.kt
+++ src/test/kotlin/menu/util/ValidationTest.kt
@@ -14,5 +14,13 @@ class ValidationTest {
         }.isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("[ERROR]")
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = ["규동,스테이크", "오니기리,끼슈,짜장면", "칼국수/하이라이스"])
+    fun `먹지 못하는 메뉴 입력 유효성 검증 테스트`(input: String) {
+        assertThatThrownBy {
+            Validation.checkCantEatMenu(input)
+        }.isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("[ERROR]")
+    }
+
 
 }
