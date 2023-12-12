15b75dfe19d60394c69d4a7efdd6706cb2654d65
gaeun <geun5744@gmail.com>
2023-12-12 19:15:49 +0900

    [FEAT(OutputView)] 메뉴 추천 결과 print 구현

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -8,5 +8,26 @@ class OutputView {
         println("점심 메뉴 추천을 시작합니다.")
     }
 
+    fun printResult(categories: List<Category>, selectMenus: Map<String, List<String>>) {
+
+        println("메뉴 추천 결과입니다.")
+        println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
+        print("[ 카테고리")
+        categories.forEach { print(" | ${it.categoryName}") }
+        println(" ]")
+
+        selectMenus.forEach { (t, u) ->
+            printCoachMenu(t, u)
+        }
+
+        println("추천을 완료했습니다.")
+    }
+
+    private fun printCoachMenu(coach: String, menus: List<String>) {
+        print("[ $coach")
+        menus.forEach { print(" | $it") }
+        println(" ]")
+    }
+
 
 }
