d050cc2b57275b94c7f6745d05961d1911c0a05b
gaeun <geun5744@gmail.com>
2023-12-12 18:02:32 +0900

    [FEAT(InputView)] 먹지 못하는 메뉴 입력 로직 구현

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -16,4 +16,28 @@ class InputView {
             readCoachesName()
         }
     }
+
+    fun readCantEat(coach: String): List<String> {
+
+        println("${coach}(이)가 못 먹는 메뉴를 입력해 주세요.")
+
+        return try {
+            val input = Console.readLine()
+            Validation.checkCantEatMenu(input)
+            input.split(",")
+        } catch (e: IllegalStateException) {
+            println(e.message)
+            readCoachesName()
+        }
+    }
+
+    fun readCantEatMenus(coaches: List<String>): Map<String, List<String>> {
+        val menus = mutableMapOf<String, List<String>>()
+
+        coaches.forEach { coach ->
+            menus[coach] = readCantEat(coach)
+        }
+
+        return menus
+    }
 }
