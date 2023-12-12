83515b96ff9905a4004719707080770f0927f1df
gaeun <geun5744@gmail.com>
2023-12-12 17:28:51 +0900

    [FIX(Validation)] 에러 메시지 문구 수정

--- src/main/kotlin/menu/util/Validation.kt
+++ src/main/kotlin/menu/util/Validation.kt
@@ -4,7 +4,7 @@ import java.lang.IllegalArgumentException
 
 object Validation {
 
-    private const val COACH_COUNT_ERROR_MESSAGE = "[ERROR] 코치는 최소 2명 이상 입력해야 합니다."
+    private const val COACH_COUNT_ERROR_MESSAGE = "[ERROR] 코치는 쉼표로 구분하여 최소 2명 이상 입력해야 합니다."
     private const val COACH_NAME_ERROR_MESSAGE = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자까지 가능합니다."
     private const val INVALID_MENU_ERROR_MESSAGE = "[ERROR] 유효하지 않은 메뉴 입력 입니다."
     private const val NAME_SEPARATOR = ","
