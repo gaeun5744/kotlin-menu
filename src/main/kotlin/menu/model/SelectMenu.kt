c997b470024a2a1f353a9951ef1d0e9fa8483e27
gaeun <geun5744@gmail.com>
2023-12-12 19:00:25 +0900

    [FIX(SelectMenu)] 메뉴 추천 로직 수정

--- src/main/kotlin/menu/model/SelectMenu.kt
+++ src/main/kotlin/menu/model/SelectMenu.kt
@@ -5,41 +5,43 @@ import menu.util.Category
 
 class SelectMenu(
     private val coachesCantEat: Map<String, List<String>>,
-    private val selectCategory: SelectCategory = SelectCategory(),
+    private val selectCategory: SelectCategory
 ) {
 
-    private val recommendation = mutableMapOf<String, MutableList<String>>().apply {
-        coachesCantEat.keys.forEach {
-            it to mutableListOf<String>()
-        }
-    }
+    private val recommendation = mutableMapOf<String, MutableList<String>>()
 
     private fun getRandomMenu(category: Category): String {
         return Randoms.shuffle(category.menu)[0]
     }
 
     fun getRecommendation(): Map<String, List<String>> {
-        val categories = selectCategory.getWeekCategories()
+        val categories = selectCategory.weekCategories
+
+        coachesCantEat.keys.forEach { coach ->
+            recommendation[coach] = mutableListOf()
+        }
 
         for (category in categories) {
             coachesCantEat.keys.forEach { coach ->
-                insertRecommendMenu(coach, category)
+                insertRecommendMenu(category, coach)
             }
         }
         return recommendation
     }
 
-    private fun insertRecommendMenu(coach: String, category: Category) {
-        while (recommendation[coach]!!.size != 5) {
+    private fun insertRecommendMenu(category: Category, coach: String) {
+        while (true) {
             val randomMenu = getRandomMenu(category)
             if (!checkCantEat(coach, randomMenu)) {
                 recommendation[coach]!!.add(randomMenu)
             }
+            break
         }
     }
 
-    private fun checkCantEat(coach: String, randomMenu: String) =
-        coachesCantEat[coach]!!.contains(randomMenu) || recommendation[coach]!!.contains(randomMenu)
+    private fun checkCantEat(coach: String, randomMenu: String): Boolean {
+        return coachesCantEat[coach]!!.contains(randomMenu) || recommendation[coach]!!.contains(randomMenu)
+    }
 
 
 }
